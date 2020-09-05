8a9557d36d656b5a4e58ddebd2afafd77d5f5986
Adam Doyle <adamldoyle@gmail.com>
2020-09-05 01:37:55 -0500

    Listen to global message rank mentions and hardcode response

--- message.js
+++ message.js
@@ -1,7 +1,18 @@
 import handler from './libs/handler-lib';
+import SlackClient from './libs/slack-lib';
 
 export const main = handler(async (event, context) => {
-  const data = JSON.parse(event.body);
+  const payload = JSON.parse(event.body);
 
-  return data;
+  if (payload.event.type === 'app_mention') {
+    const text = payload.event.text;
+    if (text.includes('what is my global message rank')) {
+      SlackClient.chat.postMessage({
+        text: `<@${payload.event.user}>: your global message rank is 2`,
+        channel: payload.event.channel,
+      });
+    }
+  }
+
+  return true;
 });
