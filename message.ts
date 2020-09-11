8bab958ce340b69b0d0c514ea133909c5fa8599e
Adam Doyle <adamldoyle@gmail.com>
2020-09-10 21:42:45 -0500

    Try something else

--- message.ts
+++ message.ts
@@ -2,19 +2,21 @@ import { handler } from './libs/handler';
 import { verifyRequest } from './libs/slack';
 import eventHandler from './events';
 
-export const main = handler(async (event) => {
-  const payload = JSON.parse(event.body);
+export const main = (event, context) => {
+  return handler(async (event) => {
+    const payload = JSON.parse(event.body);
 
-  if (!verifyRequest(event)) {
-    throw new Error('Invalid request');
-  }
+    if (!verifyRequest(event)) {
+      throw new Error('Invalid request');
+    }
 
-  if (payload.type === 'url_verification') {
-    return { challenge: payload.challenge };
-  }
-  if (payload.type === 'event_callback') {
-    return eventHandler(payload);
-  }
+    if (payload.type === 'url_verification') {
+      return { challenge: payload.challenge };
+    }
+    if (payload.type === 'event_callback') {
+      return eventHandler(payload);
+    }
 
-  return true;
-});
+    return true;
+  })(event, context);
+};
